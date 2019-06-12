8a736f407ff524e221769cced89ba7c6c98d28ce
Florian Stallmann <florian.stallmann@senacor.com>
2019-06-12 22:13:38 +0200

    adding various color helper functions for theme colors and shades

--- bkt/library/powerpoint/helpers.py
+++ bkt/library/powerpoint/helpers.py
@@ -11,7 +11,8 @@ clr.AddReference('System.Drawing')
 import System.Drawing as Drawing
 
 import json # required for tags
-from bkt import settings
+from collections import namedtuple # required for color class
+from bkt import settings # required to save global locpin setting
 
 ptToCmFactor = 2.54 / 72;
 def pt_to_cm(pt):
@@ -460,8 +461,25 @@ class ColorHelper(object):
     This class provides helper functions to handle this mess.
     '''
 
+    _theme_color_indices = [14,13,16,15, 5,6,7,8,9,10] #powerpoint default color picker is using IDs 5-10 and 13-16
+    _theme_color_names = ['Hintergrund 1', 'Text 1', 'Hintergrund 2', 'Text 2', 'Akzent 1', 'Akzent 2', 'Akzent 3', 'Akzent 4', 'Akzent 5', 'Akzent 6']
+    _theme_color_shades = [
+        # depending on HSL-Luminosity, different brightness-values are used
+        # brightness-values = percentage brighter  (darker if negative)
+        [range(0,1),     [ 50,   35,  25,  15,   5] ],
+        [range(1,51),    [ 90,   75,  50,  25,  10] ],
+        [range(51,204),  [ 80,   60,  40, -25, -50] ],
+        [range(204,255), [-10,  -25, -50, -75, -90] ],
+        [range(255,256), [ -5,  -15, -25, -35, -50] ]
+    ] #using int values to avoid floating point comparison problems
+
+    _color_class = namedtuple("ThemeColor", "rgb brightness shade_index theme_index name")
+
+
+    ### internal helper methods ###
+
     @classmethod
-    def theme_color_index_2_color_scheme_index(cls, index):
+    def _theme_color_index_2_color_scheme_index(cls, index):
         mapping = {
             13: 2,
             14: 1,
@@ -471,13 +489,13 @@ class ColorHelper(object):
         return mapping[index]
     
     @classmethod
-    def get_color_from_theme_index(cls, context, index): #expect MsoThemeColorSchemeIndex
+    def _get_color_from_theme_index(cls, context, index): #expect MsoThemeColorSchemeIndex
         if index > 12:
             try:
                 ColorScheme = context.slide.ColorScheme
             except: #if presentation has no slides, the above will fail
                 ColorScheme = context.presentation.SlideMaster.ColorScheme
-            index = cls.theme_color_index_2_color_scheme_index(index)
+            index = cls._theme_color_index_2_color_scheme_index(index)
         else:
             try:
                 ColorScheme = context.slide.ThemeColorScheme
@@ -485,6 +503,22 @@ class ColorHelper(object):
                 ColorScheme = context.presentation.SlideMaster.Theme.ThemeColorScheme
         
         return ColorScheme(index)
+
+    @classmethod
+    def _get_factors_for_rgb(cls, color_rgb):
+        color = Drawing.ColorTranslator.FromOle(color_rgb)
+        l = color.GetBrightness()*255
+        return [factors[1] for factors in cls._theme_color_shades if round(l) in factors[0]][0]
+    
+    @classmethod
+    def _get_color_name(cls, index, shade_index, brightness):
+        theme_col_name = cls._theme_color_names[cls._theme_color_indices.index(index)]
+        if brightness != 0:
+            return "{}, {} {:.0%}".format(theme_col_name, "heller" if brightness > 0 else "dunkler", abs(brightness))
+        return theme_col_name
+
+
+    ### external functions for theme colors and shades ###
 
     @classmethod
     def adjust_rgb_brightness(cls, color_rgb, brightness):
@@ -506,19 +540,48 @@ class ColorHelper(object):
         # store color rgb
         color = Drawing.Color.FromArgb(r, g, b);
         return Drawing.ColorTranslator.ToOle(color)
-
-    @classmethod
-    def get_rgb_from_theme_index(cls, context, index, brightness=0, shade_index=None):
-        color = cls.get_color_from_theme_index(context, index).RGB
-        if brightness != 0:
-            color = cls.adjust_rgb_brightness(color, brightness)
-        elif shade_index:
-            pass
-        return color
     
     @classmethod
-    def get_shade_index_from_brightness(cls, context, index, brightness):
-        pass
+    def get_brightness_from_shade_index(cls, color_rgb, shade_index):
+        factors = cls._get_factors_for_rgb(color_rgb)
+        return factors[shade_index]/100.0
+    
+    @classmethod
+    def get_shade_index_from_brightness(cls, color_rgb, brightness):
+        factors = cls._get_factors_for_rgb(color_rgb)
+        return factors.index(int(100*brightness))
+    
+    @classmethod
+    def get_theme_index(cls, i):
+        return cls._theme_color_indices[i%10]
+
+    @classmethod
+    def get_theme_color(cls, context, index, brightness=0, shade_index=None):
+        color_rgb = cls._get_color_from_theme_index(context, index).RGB
+        if shade_index is not None:
+            brightness = cls.get_brightness_from_shade_index(color_rgb, shade_index)
+        elif brightness != 0:
+            try:
+                shade_index = cls.get_shade_index_from_brightness(color_rgb, brightness)
+            except ValueError:
+                print("not found")
+                shade_index = None
+        
+        color_rgb = cls.adjust_rgb_brightness(color_rgb, brightness)
+        
+        return cls._color_class(color_rgb, brightness, shade_index, index, cls._get_color_name(index, shade_index, brightness))
+    
+    @classmethod
+    def get_theme_colors(cls):
+        return zip(cls._theme_color_indices, cls._theme_color_names)
+
+
+    ### external functions for extra colors ###
+
+    @classmethod
+    def get_extra_colors(cls, context):
+        return context.presentation.ExtraColors
+
 
 
 # =========================================
