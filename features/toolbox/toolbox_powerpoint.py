02837cb6b95165fa2a5eee1feca0dee891a76e8e
Florian Stallmann <florian.stallmann@senacor.com>
2019-09-26 18:29:05 +0200

    refactoring to toolbox generation to make it easier to create custom variations and make default ui customizable

--- features/toolbox/toolbox_powerpoint.py
+++ features/toolbox/toolbox_powerpoint.py
@@ -8,24 +8,22 @@ Created on 24.07.2014
 
 import bkt
 
-# import toolbox modules with ui
-import arrange
-import info
-import language
-import text
-import shape_adjustments
-import stateshapes
-import slides
-import shapes as mod_shapes
-import shape_selection
+
+# load function to get toolbox pages based on settings
+import toolboxui
 
 
 
+### default settings
+default_settings = {
+    "split_group": 2, #page no 2
+    "language_group": 2, #page no 2
+}
 
-# define context-menus and context-tabs
-import context_menus
-
-
+### render toolbox ui
+toolbox_ui = toolboxui.ToolboxUi(default_settings, 3)
+toolbox_ui.render_pages()
+toolbox_ui.render_contextmenus()
 
 
 # ==============================
@@ -34,47 +32,27 @@ import context_menus
 
 bkt.powerpoint.add_tab(bkt.ribbon.Tab(
     id="bkt_powerpoint_toolbox",
-    label=u'Toolbox 1/3',
+    label=toolbox_ui.get_page_name(1),
     insert_before_mso="TabHome",
     # get_visible defaults to False during async-startup
     get_visible=bkt.Callback(lambda:True),
-    children = [
-        shape_selection.clipboard_group,
-        slides.slides_group,
-        bkt.mso.group.GroupFont,
-        bkt.mso.group.GroupParagraph,
-        mod_shapes.shapes_group,
-        styles_group,
-        mod_shapes.size_group,
-        arrange.arrange_group,
-        arrange.distance_rotation_group,
-        text.innenabstand_gruppe,
-        text.paragraph_group,
-        shape_adjustments.adjustments_group,
-        info.info_group
-    ]
+    children = toolbox_ui.get_page(1),
 ))
 
 bkt.powerpoint.add_tab(bkt.ribbon.Tab(
     id="bkt_powerpoint_toolbox_advanced",
-    label=u'Toolbox 2/3',
+    label=toolbox_ui.get_page_name(2),
     insert_before_mso="TabHome",
     # get_visible defaults to False during async-startup
     get_visible=bkt.Callback(lambda:True),
-    children = [
-        mod_shapes.pos_size_group,
-        arrange.arrange_advanced_group,
-        arrange.arrange_adv_easy_group,
-        arrange.euclid_angle_group,
-        mod_shapes.format_group,
-        text.paragraph_indent_group,
-        mod_shapes.split_shapes_group,
-        language.sprachen_gruppe,
-        stateshapes.stateshape_gruppe,
-    ]
+    children = toolbox_ui.get_page(2),
 ))
 
-
-
-
+bkt.powerpoint.add_tab(bkt.ribbon.Tab(
+    id="bkt_powerpoint_toolbox_extensions",
+    label=toolbox_ui.get_page_name(3),
+    insert_before_mso="TabHome",
+    # get_visible defaults to False during async-startup
+    get_visible=bkt.Callback(lambda:True),
+), extend=True)
 
--- features/toolbox_widescreen/my_toolbox.py
+++ features/toolbox_widescreen/my_toolbox.py
@@ -8,23 +8,33 @@ Created on 24.07.2014
 
 import bkt
 
-# import toolbox modules with ui
-from toolbox import arrange, info, language, text, shape_adjustments, stateshapes, slides, shapes as mod_shapes, shape_selection
-
 
 # FIXME: this should be easier
 from os.path import dirname, join, normpath, realpath
 bkt.apps.Resources.root_folders.append(normpath(join(dirname(realpath(__file__)), '..', 'toolbox', 'resources')))
+# ###
 
 
-
-# define context-menus and context-tabs
-from toolbox import context_menus
+from toolbox import toolboxui, info
 
 # tab id for tab activator workaround
 info.TabActivator.tab_id = "my_bkt_powerpoint_toolbox"
 
 
+### default settings
+default_settings = {
+    "tab_name": "BKT", #Tab Name
+    "size_group": 0, #off
+    "pos_size_group": 1, #page no 1
+    "arrange_mini_group": 1, #page no 1
+    "arrange_adv_easy_group": 0, #off
+    "text_parindent_group": 1, #page no 1
+}
+
+### render toolbox ui
+toolbox_ui = toolboxui.ToolboxUi(default_settings, 2)
+toolbox_ui.render_pages()
+toolbox_ui.render_contextmenus()
 
 
 # ==============================
@@ -33,28 +43,11 @@ info.TabActivator.tab_id = "my_bkt_powerpoint_toolbox"
 
 bkt.powerpoint.add_tab(bkt.ribbon.Tab(
     id="my_bkt_powerpoint_toolbox",
-    label=u'BKT 1/2',
+    label=toolbox_ui.get_page_name(1),
     insert_before_mso="TabHome",
     # get_visible defaults to False during async-startup
     get_visible=bkt.Callback(lambda:True),
-    children = [
-        shape_selection.clipboard_group,
-        slides.slides_group,
-        bkt.mso.group.GroupFont,
-        bkt.mso.group.GroupParagraph,
-        mod_shapes.shapes_group,
-        styles_group,
-        #mod_shapes.size_group,
-        mod_shapes.pos_size_group,
-        arrange.arrange_group,
-        arrange_advanced_small_group,
-        arrange.distance_rotation_group,
-        text.innenabstand_gruppe,
-        text.paragraph_group,
-        text.paragraph_indent_group,
-        shape_adjustments.adjustments_group,
-        info.info_group
-    ]
+    children = toolbox_ui.get_page(1),
 ))
 
 bkt.powerpoint.add_tab(bkt.ribbon.Tab(
@@ -62,18 +55,10 @@ bkt.powerpoint.add_tab(bkt.ribbon.Tab(
     #id_q="nsBKT:powerpoint_toolbox_extensions",
     #insert_after_q="nsBKT:powerpoint_toolbox_advanced",
     insert_before_mso="TabHome",
-    label=u'BKT 2/2',
+    label=toolbox_ui.get_page_name(2),
     # get_visible defaults to False during async-startup
     get_visible=bkt.Callback(lambda:True),
-    children = [
-        arrange.arrange_advanced_group,
-        arrange.euclid_angle_group,
-        mod_shapes.format_group,
-        #arrange.arrange_adv_easy_group,
-        #mod_shapes.split_shapes_group,
-        #language.sprachen_gruppe,
-        stateshapes.stateshape_gruppe,
-    ]
+    children = toolbox_ui.get_page(2),
 ), extend=True)
 
 
