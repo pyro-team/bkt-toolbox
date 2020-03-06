6248ad66a533f8e54ec928dbeed7e7eb02501906
Florian Stallmann <florian.stallmann@senacor.com>
2020-03-06 19:22:21 +0100

    devkit: added image-mso search

--- features/devkit/common.py
+++ features/devkit/common.py
@@ -188,19 +188,132 @@ common_group = bkt.ribbon.Group(
 
 
 class ImageMso(object):
-    all_images = None
+    search_limit = 100
+
+    search_engine = None
+    search_term = ""
+    search_results = None
 
     @classmethod
-    def load_json(cls):
+    def load_json(cls, search_engine):
         import os.path
         import io
         import json
 
+        search_writer = search_engine.writer()
         file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "imagemso.json")
         with io.open(file, 'r', encoding='utf-8') as json_file:
-            cls.all_images = json.load(json_file)
+            for icon in json.load(json_file):
+                search_writer.add_document(module="imagemso", name=icon, keywords=icon)
+            search_writer.commit()
+
+    @classmethod
+    def get_search_engine(cls):
+        if cls.search_engine:
+            return cls.search_engine
+
+        from bkt.search import get_search_engine
+        cls.search_engine = get_search_engine("imagemso")
+        # initialize search index on first use of engine
+        cls.load_json(cls.search_engine)
+        return cls.search_engine
+    
+    @classmethod
+    def _perform_search(cls):
+        if len(cls.search_term) > 0:
+            cls.search_results = None
+            engine = cls.get_search_engine()
+            with engine.searcher() as searcher:
+                    cls.search_results = searcher.search(cls.search_term)
+        else:
+            cls.search_results = None
+
+    @classmethod
+    def set_search_term(cls, value):
+        cls.search_term = value
+        cls._perform_search()
+
+    @classmethod
+    def get_search_term(cls):
+        return cls.search_term
+    
+    @classmethod
+    def get_symbol_galleries(cls):
+        if not cls.search_results or len(cls.search_results) == 0:
+            image_msos = [
+                bkt.ribbon.Button(
+                    label="Keine Ergebnisse für »{}«".format(cls.search_term),
+                    enabled=False
+                )
+            ]
+        
+        else:
+            image_msos = [
+                bkt.ribbon.MenuSeparator(title="{} Ergebnisse für »{}«".format(len(cls.search_results), cls.search_term))
+            ]
+            if len(cls.search_results) > cls.search_limit:
+                image_msos.append(
+                    bkt.ribbon.Button(label="Zeige die ersten {} Ergebnisse".format(cls.search_limit), enabled=False)
+                )
+            for icon in cls.search_results.limit(cls.search_limit):
+                image_msos.append(
+                    bkt.ribbon.Button(
+                        label=icon.name,
+                        image_mso=icon.name,
+                        on_action=bkt.Callback(cls.copy_to_clipboard)
+                    )
+                )
+        
+        return bkt.ribbon.Menu(
+                    xmlns="http://schemas.microsoft.com/office/2009/07/customui",
+                    id=None,
+                    children=image_msos
+                )
+
+    @classmethod
+    def get_enabled_results(cls):
+        return cls.search_results is not None
+    
+    @classmethod
+    def get_results_label(cls):
+        if cls.search_results is not None:
+            return "{} Icons".format(len(cls.search_results))
+        else:
+            return "Ergebnis"
+
+    @classmethod
+    def copy_to_clipboard(cls, current_control):
+        import bkt.dotnet as dotnet
+        Forms = dotnet.import_forms() #required to read clipboard
+        Forms.Clipboard.SetText( current_control['label'] )
 
 
 
+iconsearch_group = bkt.ribbon.Group(
+    id="bkt_common_dev_iconsearch_group",
+    label="ImageMso",
+    image_mso='SearchUI',
+    children=[
+        bkt.ribbon.Label(
+            label="Suchwort:",
+        ),
+        bkt.ribbon.EditBox(
+            label="Suchwort",
+            show_label=False,
+            sizeString = '#########',
+            get_text = bkt.Callback(ImageMso.get_search_term),
+            on_change = bkt.Callback(ImageMso.set_search_term),
+            supertip="Suchwort eingeben und ENTER klicken",
+        ),
+        bkt.ribbon.DynamicMenu(
+            get_label=bkt.Callback(ImageMso.get_results_label),
+            get_content=bkt.Callback(ImageMso.get_symbol_galleries),
+            get_enabled=bkt.Callback(ImageMso.get_enabled_results),
+            screentip="Suchergebnisse",
+            supertip="Zeigt die Suchergebnisse der Icon-Suche nach dem gewünschten Suchwort an",
+        ),
+    ]
+)
 
-common_groups = [common_group]
+
+common_groups = [common_group, iconsearch_group]
