eb5ee85e1a8285d9c1863da96dc2e99073522314
Florian Stallmann <florian.stallmann@gmail.com>
2020-03-08 18:52:57 +0100

    remove legacy code for bkt decorators

--- bkt/__init__.py
+++ bkt/__init__.py
@@ -39,59 +39,59 @@ from bkt.library.general import (
 )
 
 #@deprecated
-from bkt.decorators import (
-    # public classes
-    #EventHandler,
-    #CallableContextInformation,
-    #Resources,
+# from bkt.decorators import (
+#     # public classes
+#     #EventHandler,
+#     #CallableContextInformation,
+#     #Resources,
     
-    # decorators for ribbon-classes
-    uicontrol,
-    use,
-    tab,
-    group,
-    menu,
-    box,
-    button,
-    large_button,
-    toggle_button,
-    edit_box,
-    spinner_box,
-    gallery,
-    combo_box,
-    #dialog_box_launcher,
-    #check_box,
-    #incdec_edit_box,
+#     # decorators for ribbon-classes
+#     uicontrol,
+#     use,
+#     tab,
+#     group,
+#     menu,
+#     box,
+#     button,
+#     large_button,
+#     toggle_button,
+#     edit_box,
+#     spinner_box,
+#     gallery,
+#     combo_box,
+#     #dialog_box_launcher,
+#     #check_box,
+#     #incdec_edit_box,
     
-    # decorators for ribbon attributes
-    configure,
-    image,
-    image_mso,
+#     # decorators for ribbon attributes
+#     configure,
+#     image,
+#     image_mso,
     
-    # decorators for callbacks
-    callback_type,
-    increment,
-    decrement,
-    on_change,
-    get_text,
+#     # decorators for callbacks
+#     callback_type,
+#     increment,
+#     decrement,
+#     on_change,
+#     get_text,
     
-    # decorators for context
-    arg_python_addin,
-    arg_ribbon_id,
-    arg_context,
-    arg_presentation,
-    arg_shape,
-    arg_shapes,
-    arg_shapes_limited,
-    arg_slide,
-    arg_slides,
-    arg_slides_limited,
-    arg_page_shapes,
-    require_text,
-    no_transaction,
-    uuid
-    # decorators for office apps
-)
+#     # decorators for context
+#     arg_python_addin,
+#     arg_ribbon_id,
+#     arg_context,
+#     arg_presentation,
+#     arg_shape,
+#     arg_shapes,
+#     arg_shapes_limited,
+#     arg_slide,
+#     arg_slides,
+#     arg_slides_limited,
+#     arg_page_shapes,
+#     require_text,
+#     no_transaction,
+#     uuid
+#     # decorators for office apps
+# )
 
 from bkt.appui import (excel,
                       outlook,
