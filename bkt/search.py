1985bd747be5b75a98039e951eec71e09368b44f
Florian Stallmann <florian.stallmann@gmail.com>
2020-03-30 19:43:26 +0200

    further improvement of search engine by replacing fnmatch with in-operator and support AND and OR joins

--- bkt/search.py
+++ bkt/search.py
@@ -12,11 +12,13 @@ from __future__ import absolute_import
 import logging
 
 import re #for extracting keywords from string
-import fnmatch #for fuzzy search in list
+# import fnmatch #for fuzzy search in list
 
-from collections import defaultdict, namedtuple, OrderedDict
+from itertools import groupby, islice, chain
+from collections import namedtuple, OrderedDict, deque
 from contextlib import contextmanager
 
+from bkt import settings
 
 ### default search document
 SearchDocument = namedtuple("SearchDocument", "module name keywords")
@@ -50,7 +52,6 @@ class SearchResults(object):
         self._chained_iterators = value
 
     def groupedby(self, field):
-        from itertools import groupby
         self._iterator = [(k,list(g)) for k, g in groupby(self._iterator, key=lambda d: getattr(d, field))]
         return self
         # result_dict = OrderedDict()
@@ -66,7 +67,6 @@ class SearchResults(object):
         return self
 
     def limit(self, stop, start=0):
-        from itertools import islice
         self._iterator = islice(self._iterator, stop=stop, start=start)
         return self
 
@@ -107,7 +107,7 @@ class SearchWriter(object):
             logging.debug("SEARCH: commit document {} for keywords: {}".format(doc_hash, keywords))
             for keyword in keywords:
                 self._engine._keywords.add(keyword)
-                self._engine._db[hash(keyword)].add(doc_hash)
+                self._engine._db.setdefault(hash(keyword), set()).add(doc_hash)
 
         del self
     
@@ -120,59 +120,124 @@ class SearchSearcher(object):
         self._engine = engine
 
     ### SEARCH ENGINE ###
-    def search(self, query):
-        ''' search with wildcards around each keyword, multiple keywords are connected with OR '''
+    def search(self, query, join_and=True):
+        ''' search with wildcards around each keyword
+            join_and=True > multiple keywords are connected with AND
+            join_and=False > multiple keywords are connected with OR
+        '''
         logging.debug("SEARCH: for "+query)
+
+        #add to search history
+        self._engine.add_to_recent(query)
 
         # #check min length
         # if len(query) < 3:
         #     return result #empty result
         
         #split keyword by whitespace
-        search_terms = set(["*{}*".format(s) for s in query.lower().split()])
+        # search_terms = set(["*{}*".format(s) for s in query.lower().split()])
+        # search_patterns = [re.compile(".*"+s+".*") for s in query.lower().split()]
+        search_terms = set(query.lower().split())
 
         #perform search with wildcards using fnmatch
-        #FIXME: consider using regex instead of fnmatch?
-        result_keywords = set()
-        for search_term in search_terms:
-            result_keywords = result_keywords.union(fnmatch.filter(self._engine._keywords, search_term))
+        # result_keywords = []
+        # for search_term in search_terms:
+        #     result_keywords.append(
+        #         fnmatch.filter(self._engine._keywords, search_term)
+        #     )
+        #     #result_keywords = result_keywords.union(fnmatch.filter(self._engine._keywords, search_term))
         
-        logging.debug("SEARCH: found keywords "+",".join(result_keywords))
+        #perform search using regex
+        # result_keywords = {}
+        # for keyword in self._engine._keywords:
+        #     for pattern in search_patterns:
+        #         if pattern.match(keyword):
+        #             result_keywords.setdefault(pattern.pattern, []).append(keyword)
+
+        #perform search using in operator
+        # result_keywords = []
+        # for search_term in search_terms:
+        #     result_keywords.append([
+        #         k for k in self._engine._keywords if search_term in k
+        #     ])
+        #fastest version with generator expression
+        result_keywords = (
+            [
+                k for k in self._engine._keywords if search_term in k
+            ] for search_term in search_terms
+        )
+        
+        # logging.debug("SEARCH: found keywords "+",".join(chain.from_iterable(result_keywords)))
 
         #convert found keywords to document hashs
-        result_doc_hashes = set()
-        for keyword in result_keywords:
-            # result_doc_hashes = result_doc_hashes.union(self._engine._db[hash(keyword)])
-            result_doc_hashes.update(self._engine._db[hash(keyword)])
+        #AND-search
+        if join_and:
+            #add document hashes that have all defined keywords
+            try:
+                keyword_doc_hashes = []
+                for search_terms in result_keywords:
+                    #search doc hashes for current keyword
+                    current_doc_hashes = set()
+                    for keyword in search_terms:
+                        current_doc_hashes.update(self._engine._db[hash(keyword)])
+                    keyword_doc_hashes.append(current_doc_hashes)
+                #add doc hashes of all keyword doc hashes using intersection (AND-search)
+                result_doc_hashes = set.intersection(*keyword_doc_hashes)
+            except KeyError:
+                logging.debug("SEARCH: empty result set due to KeyError")
+                result_doc_hashes = set() #emptyset
+        
+        #OR-search
+        else:
+            #add document hashes that have any defined keywords
+            result_doc_hashes = set()
+            for keyword in chain.from_iterable(result_keywords): 
+                #chain unpacks nesting of lists
+                result_doc_hashes.update(self._engine._db.get(hash(keyword), set()))
 
         #add results in the order that documents have been added
         # for doc_hash,doc in self._engine._docs.iteritems():
         #     if doc_hash in result_doc_hashes:
         #         result.add_result(doc)
-        
+
         return SearchResults(self._engine._docs, result_doc_hashes)
     
-    def search_exact(self, query):
-        ''' exact search without wildcards, multiple keywords are connected with AND '''
+    def search_exact(self, query, join_and=True):
+        ''' exact search without wildcards
+            join_and=True > multiple keywords are connected with AND
+            join_and=False > multiple keywords are connected with OR
+        '''
         logging.debug("SEARCH EXACT: for "+query)
+
+        #add to search history
+        self._engine.add_to_recent(query)
         
         #split keyword by whitespace
-        search_terms = set([s for s in query.lower().split()])
+        search_terms = set(query.lower().split())
 
-        #add document hashes that have all defined keywords
-        #NOTE: as _db is a defaultdict, unkown search_terms do not throw an error but return an empty set
-        result_doc_hashes = self._engine._db[hash(search_terms.pop())]
-        for keyword in search_terms:
-            # result_doc_hashes = result_doc_hashes.intersection(self._engine._db[hash(keyword)])
-            result_doc_hashes.intersection_update(self._engine._db[hash(keyword)])
+        #AND-search
+        if join_and:
+            #add document hashes that have all defined keywords
+            try:
+                result_doc_hashes = self._engine._db[hash(search_terms.pop())]
+                for keyword in search_terms:
+                    result_doc_hashes.intersection_update(self._engine._db[hash(keyword)])
+            except KeyError:
+                logging.debug("SEARCH EXACT: empty result set due to KeyError")
+                result_doc_hashes = set() #emptyset
+        
+        #OR-search
+        else:
+            #add document hashes that have any defined keywords
+            result_doc_hashes = set()
+            for keyword in search_terms:
+                result_doc_hashes.update(self._engine._db.get(hash(keyword), set()))
 
         #add results in the order that documents have been added
         # for doc_hash,doc in self._engine._docs.iteritems():
         #     if doc_hash in result_doc_hashes:
         #         result.add_result(doc)
 
-        # for doc_hash in self._engine._db[hash(query.lower())]:
-        #     result.add_result(self._engine._docs[doc_hash])
         return SearchResults(self._engine._docs, result_doc_hashes)
 
 
@@ -181,7 +246,7 @@ class SearchEngine(object):
         self._name = name
         self._schema = schema
 
-        self._db = defaultdict(set)
+        self._db = dict()
         self._docs = OrderedDict()
         self._keywords = set()
 
