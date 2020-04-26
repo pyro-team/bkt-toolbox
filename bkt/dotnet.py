1534dd44115621bea3f289b207e1d4e113eae810
Florian Stallmann <florian.stallmann@gmail.com>
2020-04-26 20:03:36 +0200

    added memoize pattern for all dotnet imports

--- bkt/dotnet.py
+++ bkt/dotnet.py
@@ -6,60 +6,82 @@ Created on 23.11.2014
 @author: cschmitt
 '''
 
-from __future__ import absolute_import
+from __future__ import absolute_import, print_function
 
+from functools import wraps
+
+cache = {}
+def memoize(func):
+    # cache = {}
+    @wraps(func)
+    def memoizer(*args):
+        if not func in cache:
+            cache[func] = func(*args)
+        return cache[func]
+    return memoizer
+
+@memoize
 def import_linq():
     import clr
     clr.AddReference('System.Xml.Linq')
     import System.Xml.Linq as xml
     return xml
 
+@memoize
 def import_forms():
     import clr
     clr.AddReference('System.Windows.Forms')
     import System.Windows.Forms as forms
     return forms
 
+@memoize
 def import_drawing():
     import clr
     clr.AddReference('System.Drawing')
     import System.Drawing as drawing
     return drawing
 
+@memoize
 def import_win32():
     import Microsoft.Win32 as win32
     return win32
 
+@memoize
 def import_powerpoint():
     import clr
     clr.AddReference('Microsoft.Office.Interop.PowerPoint')
     import Microsoft.Office.Interop.PowerPoint as powerpoint
     return powerpoint
 
+@memoize
 def import_excel():
     import clr
     clr.AddReference('Microsoft.Office.Interop.Excel')
     import Microsoft.Office.Interop.Excel as excel
     return excel
 
+@memoize
 def import_outlook():
     import clr
     clr.AddReference('Microsoft.Office.Interop.Outlook')
     import Microsoft.Office.Interop.Outlook as outlook
     return outlook
 
+@memoize
 def import_officecore():
     import clr
     clr.AddReference('Office')
     import Microsoft.Office.Core as officecore
     return officecore
 
+@memoize
 def import_wpf():
     import clr
     clr.AddReference("IronPython.Wpf")
     import wpf as wpf
     return wpf
 
+@memoize
 def import_bkt():
     import clr
     clr.AddReference("BKT")
