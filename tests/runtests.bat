27d60ea9ef1758fee077f14086afe30ed5de8ee4
Florian Stallmann <florian.stallmann@gmail.com>
2020-05-13 12:56:49 +0200

    fixed taskpane test

--- tests/runtests.bat
+++ tests/runtests.bat
@@ -1,1 +1,11 @@
-bin\ipy-2.7.8\ipy.exe -m unittest discover
+@echo off
+%~d0
+cd %~dp0
+cd ..
+@echo on
+
+REM bin\ipy.exe -m unittest discover
+REM pause
+
+bin\ipy.exe -m unittest tests.test_taskpane
+pause
