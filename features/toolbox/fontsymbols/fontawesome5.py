8e600e099c76f596d508e388916e8e5da4f3e0f1
Florian Stallmann <mail@florian-stallmann.de>
2022-01-31 22:29:51 +0100

    updated font awesome 5 definition to 5.15.4

--- features/toolbox/fontsymbols/fontawesome5.py
+++ features/toolbox/fontsymbols/fontawesome5.py
@@ -19,7 +19,7 @@ from bkt.library.powerpoint import PPTSymbolsGallery
 # DO NOT USE metadata/icons.json!
 ###
 
-version_of_fontawesome_json = "5.15.0"
+version_of_fontawesome_json = "5.15.4"
 menu_title = 'Font Awesome 5 Free v' + version_of_fontawesome_json
 
 # full font names
