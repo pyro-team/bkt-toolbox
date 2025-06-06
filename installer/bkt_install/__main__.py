c9f17bcc5fedafcdcb7c97ef3c72977ee42b9a53
Florian Stallmann <florian@stallmann.me>
2025-06-06 13:58:11 +0200

    fixing installer arguments and powerpoint activation

--- installer/bkt_install/__main__.py
+++ installer/bkt_install/__main__.py
@@ -34,29 +34,32 @@ class BktInstaller(object):
 
 
 parser = argparse.ArgumentParser(prog="bkt_install", description='BKT install and configuration scripts')
-parser.add_argument('-s', '--silent', action='store_true', help='Do not confirm any questions or deletion')
 
 subparsers = parser.add_subparsers(help='BKT Installer supports 4 modes: install, uninstall, configure, and cleanup')
 
-parser_install = subparsers.add_parser('install', help='Installation and registration of BKT')
+# Create a parent parser for common arguments
+parent_parser = argparse.ArgumentParser(add_help=False)
+parent_parser.add_argument('-s', '--silent', action='store_true', help='Do not confirm any questions or deletion')
+
+parser_install = subparsers.add_parser('install', parents=[parent_parser], help='Installation and registration of BKT')
 parser_install.add_argument('--register_only', action='store_true', help='Only register addin without creating or updating default configuration')
 parser_install.add_argument('--force_office_bitness', choices=['32', '64', 'x64', 'x86'], help='On 64-bit windows skip auto check for 32/64 bit office version and force particular bitness')
-parser_install.add_argument('--apps', nargs='+', choices=["excel", "word", "outlook", "visio"], default=['powerpoint'], help='Define list of application(s) in which BKT is activated by default (in addition to PowerPoint)')
+parser_install.add_argument('--apps', nargs='+', choices=["powerpoint", "excel", "word", "outlook", "visio"], default=['powerpoint'], help='Define list of application(s) in which BKT is activated by default (in addition to PowerPoint)')
 parser_install.add_argument('--add_to_dndlist', action='store_true', help='Add the BKT AddIn to DoNotDisableAddinList in resiliency')
 parser_install.set_defaults(func=BktInstaller.install)
 
-parser_uninstall = subparsers.add_parser('uninstall', help='Remove registration of BKT')
+parser_uninstall = subparsers.add_parser('uninstall', parents=[parent_parser], help='Remove registration of BKT')
 parser_uninstall.add_argument('--remove_config', action='store_true', help='Remove config.txt file')
 parser_uninstall.set_defaults(func=BktInstaller.uninstall)
 
-parser_configure = subparsers.add_parser('configure', help='Edit BKT configuration')
+parser_configure = subparsers.add_parser('configure', parents=[parent_parser], help='Edit BKT configuration')
 parser_configure.add_argument('--set_config', metavar=('KEY','VALUE'), nargs=2, action='append', help='Add or update KEY to VALUE in config.txt')
 parser_configure.add_argument('--add_folders', metavar=('PATH1','PATH2'), nargs='+', help='Add feature folder (absolute path or path relative to bkt install folder) to config file')
 parser_configure.add_argument('--remove_folders', metavar=('PATH1','PATH2'), nargs='+', help='Remove feature folder (absolute path or path relative to bkt install folder) from config file')
 parser_configure.add_argument('--migrate_from', metavar='OLD_VERSION', help='Migrate config.txt from the given version to the current one')
 parser_configure.set_defaults(func=BktInstaller.configure)
 
-parser_cleanup = subparsers.add_parser('cleanup', help='Perform clean-up tasks to fix problems with BKT')
+parser_cleanup = subparsers.add_parser('cleanup', parents=[parent_parser], help='Perform clean-up tasks to fix problems with BKT')
 parser_cleanup.add_argument('--clear_cache', action='store_true', help='Clear all caches')
 parser_cleanup.add_argument('--clear_config', action='store_true', help='Clear config.txt file')
 parser_cleanup.add_argument('--clear_settings', action='store_true', help='Clear all app settings')
@@ -66,8 +69,11 @@ parser_cleanup.set_defaults(func=BktInstaller.cleanup)
 
 args = parser.parse_args()
 
-if not 'IronPython' in platform.python_implementation():
-    raise SystemError("BKT-Installer needs to be run by IronPython")
+if not hasattr(args, 'func'):
+    parser.print_help()
 else:
-    print("Loading BKT-Installer running IronPython %s\n" % platform.python_version())
-    args.func(args)
+    if not 'IronPython' in platform.python_implementation():
+        raise SystemError("BKT-Installer needs to be run by IronPython")
+    else:
+        print("Loading BKT-Installer running IronPython %s\n" % platform.python_version())
+        args.func(args)
