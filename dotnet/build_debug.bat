339cc44ef30d269b6ffaee50f0a57af99003bfd1
Florian Stallmann <florian@stallmann.me>
2026-02-04 22:49:25 +0100

    use modern compiler in build scripts

--- dotnet/build_debug.bat
+++ dotnet/build_debug.bat
@@ -1,21 +1,48 @@
 
-:: FIND MSBUILD.EXE in C:\Windows\Microsoft.Net\Framework
-:: LOOK FOR FRAMEWORK VERSION 4.5, 4.0 or 3.5
-:: see http://stackoverflow.com/a/17736442, http://stackoverflow.com/a/23028195
+:: FIND MSBUILD.EXE
+:: First try Visual Studio 2022/2019 MSBuild (supports modern C#)
+:: Then fall back to .NET Framework MSBuild
 @echo off
-set "framework_path="
-for /d %%a in ("C:\Windows\Microsoft.Net\Framework\*") do if "%%~nxa"=="v4.0.30319" (set "framework_path=%%a")
-if "%framework_path%"=="" (
-	for /d %%a in ("C:\Windows\Microsoft.Net\Framework\*") do if "%%~nxa"=="v4.0" (set "framework_path=%%a")
+set "msbuild_path="
+
+:: Try VS 2022 (various editions)
+for %%e in (Enterprise Professional Community BuildTools) do (
+    if "%msbuild_path%"=="" if exist "C:\Program Files\Microsoft Visual Studio\2022\%%e\MSBuild\Current\Bin\MSBuild.exe" (
+        set "msbuild_path=C:\Program Files\Microsoft Visual Studio\2022\%%e\MSBuild\Current\Bin\MSBuild.exe"
+    )
 )
-if "%framework_path%"=="" (
-	for /d %%a in ("C:\Windows\Microsoft.Net\Framework\*") do if "%%~nxa"=="v3.5" (set "framework_path=%%a")
+
+:: Try VS 2022 (various editions)
+for %%e in (Enterprise Professional Community BuildTools) do (
+    if "%msbuild_path%"=="" if exist "C:\Program Files (x86)\Microsoft Visual Studio\2022\%%e\MSBuild\Current\Bin\MSBuild.exe" (
+        set "msbuild_path=C:\Program Files (x86)\Microsoft Visual Studio\2022\%%e\MSBuild\Current\Bin\MSBuild.exe"
+    )
 )
+
+:: Try VS 2019 (various editions)
+for %%e in (Enterprise Professional Community BuildTools) do (
+    if "%msbuild_path%"=="" if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\%%e\MSBuild\Current\Bin\MSBuild.exe" (
+        set "msbuild_path=C:\Program Files (x86)\Microsoft Visual Studio\2019\%%e\MSBuild\Current\Bin\MSBuild.exe"
+    )
+)
+
+:: Fall back to .NET Framework MSBuild (older C# compiler)
+if "%msbuild_path%"=="" (
+    for /d %%a in ("C:\Windows\Microsoft.Net\Framework\*") do if "%%~nxa"=="v4.0.30319" (set "msbuild_path=%%a\msbuild.exe")
+)
+
+if "%msbuild_path%"=="" (
+    echo ERROR: MSBuild not found!
+    pause
+    exit /b 1
+)
+
+echo Using MSBuild: %msbuild_path%
 
 
 :: RUN MSBUILD
 @echo on
-%framework_path%\msbuild bkt.sln /t:Rebuild /p:Configuration=Debug "/p:Platform=Any cpu"
+"%msbuild_path%" bkt.sln /t:Rebuild /p:Configuration=Debug "/p:Platform=Any cpu"
 
 
 :: COPY FILES
